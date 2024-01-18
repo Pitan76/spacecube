4092c0fda5a1c7959030d2b57b7f5d718f90615a
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-01-18 18:09:51 +0900

    fix models

--- src/main/java/net/pitan76/spacecube/client/SpaceCubeClient.java
+++ src/main/java/net/pitan76/spacecube/client/SpaceCubeClient.java
@@ -1,11 +1,19 @@
 package net.pitan76.spacecube.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.block.Block;
+import net.minecraft.client.render.RenderLayer;
+import net.pitan76.spacecube.Blocks;
 
 public class SpaceCubeClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        setCutoutLayer(Blocks.TUNNEL_WALL);
+    }
 
+    private static void setCutoutLayer(Block block) {
+        BlockRenderLayerMap.INSTANCE.putBlock(block, RenderLayer.getCutout());
     }
 }
