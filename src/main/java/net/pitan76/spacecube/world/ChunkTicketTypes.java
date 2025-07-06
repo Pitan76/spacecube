af4b6d50b69a4e196762590c6a3c8d25e9992398
PTOM76 <ptms76@outlook.jp>
2025-07-06 18:48:02 +0900

    fix: update ChunkTicketTypes initialization to use new registry method

--- src/main/java/net/pitan76/spacecube/world/ChunkTicketTypes.java
+++ src/main/java/net/pitan76/spacecube/world/ChunkTicketTypes.java
@@ -4,10 +4,12 @@ import net.minecraft.util.math.ChunkPos;
 import net.pitan76.mcpitanlib.midohra.world.chunk.ChunkTicketType;
 import net.pitan76.spacecube.SpaceCube;
 
+import static net.pitan76.spacecube.SpaceCube.registry;
+
 public class ChunkTicketTypes {
     public static final ChunkTicketType<ChunkPos> CHUNK_LOADER = ChunkTicketType.create("spacecube:chunk_loader");
 
     public static void init() {
-        SpaceCube.registry.registerChunkTicketType(() -> CHUNK_LOADER);
+        registry.registerChunkTicketType(SpaceCube._id("chunk_loader").toMinecraft(), () -> CHUNK_LOADER);
     }
 }
