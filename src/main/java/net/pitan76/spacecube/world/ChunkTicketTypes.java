57e632796af227d4c858df641c383d4293f56e43
PTOM76 <ptms76@outlook.jp>
2025-07-12 12:19:58 +0900

    fix: update ChunkTicketTypes to use RegistryResult for improved ticket handling

--- src/main/java/net/pitan76/spacecube/world/ChunkTicketTypes.java
+++ src/main/java/net/pitan76/spacecube/world/ChunkTicketTypes.java
@@ -1,13 +1,14 @@
 package net.pitan76.spacecube.world;
 
+import net.pitan76.mcpitanlib.api.registry.result.RegistryResult;
 import net.pitan76.mcpitanlib.midohra.world.chunk.ChunkTicketType;
 import net.pitan76.spacecube.SpaceCube;
 
 import static net.pitan76.spacecube.SpaceCube.registry;
 
 public class ChunkTicketTypes {
-    public static ChunkTicketType<?> CHUNK_LOADER = registry.registerChunkTicketType(SpaceCube._id("chunk_loader"),
-            () -> ChunkTicketType.create("spacecube:chunk_loader")).get();
+    public static RegistryResult<ChunkTicketType<?>> CHUNK_LOADER = registry.registerChunkTicketType(SpaceCube._id("chunk_loader"),
+            () -> ChunkTicketType.create("spacecube:chunk_loader"));
 
     public static void init() {
 
